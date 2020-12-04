7846c80d248f17ca4eebe1005d8cb6a03f0bd79e
Belgan <IvanMedvedev900@yandex.ru>
2020-12-04 03:19:26 +0300

    Now system can launch

--- kern/timer.c
+++ kern/timer.c
@@ -105,40 +105,65 @@ get_rsdp(void) {
   return krsdp;
 }
 
-// LAB 5 code
+// LAB 5: Your code here.
+// Obtain and map FADT ACPI table address.
 static void * acpi_find_table(const char * sign) {
-  static RSDT * krsdt;
+  static RSDT * krsdt;  //Структура RSDP содержит указатель на таблицу RSDT (RootSystem Description Table), содержащую каталог таблиц ACPI
   static size_t krsdt_len;
   static size_t krsdt_entsz;
- 
-  // uint8_t cksm = 0;
+  
+  uint8_t cksm = 0;
 
   if (!krsdt) {
-    if (!uefi_lp->ACPIRoot) {
-      panic("No rsdp\n");
+    if (!uefi_lp->ACPIRoot) {  // uefi_lp is LOADER_PARAMS
+      panic("No rsdp\n");   // fadt = Fixed ACPI Description Table) The FADT is pointed to by an entry in the RSDT.
     }
     RSDP * krsdp = mmio_map_region(uefi_lp->ACPIRoot, sizeof(RSDP));
- 
+    //The ACPI Version can be detected using the Revision field in the RSDP. If this field contains 0, then ACPI Version 1.0 is used.   //For subsequent versions (ACPI version 2.0 to 6.1), the value 2 is used [1]. The exact version of ACPI can be deduced via the FADT table.
+  //   
+    if (strncmp(krsdp->Signature, "RSD PTR", 8))
+      // panic("Invalid RSDP");
+
+    for (size_t i = 0; i < offsetof(RSDP, Length); i++)
+      cksm = (uint8_t)(cksm + ((uint8_t *)krsdp)[i]);
+    if (cksm)
+      panic("Invalid RSDP");
     uint64_t rsdt_pa = krsdp->RsdtAddress;
     krsdt_entsz = 4;
-    if (krsdp->Revision) {
+    if (krsdp->Revision) { //версия ACPI не 1.0
       /* ACPI version >= 2.0 */
-      rsdt_pa = krsdp->XsdtAddress;
+      for (size_t i = 0; i < krsdp->Length; i++)
+        cksm = (uint8_t)(cksm + ((uint8_t *)krsdp)[i]);
+      if (cksm)
+        panic("Invalid RSDP");
+      rsdt_pa = krsdp->XsdtAddress; // адрес XSDT (расширенной RSDT)
       krsdt_entsz = 8;
     }
 
-    krsdt = mmio_map_region(rsdt_pa, sizeof(RSDT));
+    krsdt = mmio_map_region(rsdt_pa, sizeof(RSDT)); // параметры: адрес XSDT и размер таблицы RSDT
     /* Remap since we can obtain table length only after mapping */
-    krsdt = mmio_map_region(rsdt_pa, krsdt->h.Length);
+    krsdt = mmio_remap_last_region(rsdt_pa, krsdt, sizeof(RSDP), krsdt->h.Length);
 
-    krsdt_len = (krsdt->h.Length - sizeof(RSDT)) / 4;
+    for (size_t i = 0; i < krsdt->h.Length; i++)
+      cksm = (uint8_t)(cksm + ((uint8_t *)krsdt)[i]);
+
+    if (cksm)
+      panic("Invalid RSDP");
+
+    if (strncmp(krsdt->h.Signature, krsdp->Revision ? "XSDT" : "RSDT", 4))
+      panic("Invalid RSDT");
+
+
+    krsdt = mmio_map_region(rsdt_pa, krsdt->h.Length); // параметры: адрес XSDT и размер зголовка ACPI
+
+    krsdt_len = (krsdt->h.Length - sizeof(RSDT)) / 4; // количество записей в rsdt(xsdt)
     if (krsdp->Revision) {
       krsdt_len = krsdt_len / 2;
     }
   }
 
   ACPISDTHeader * hd = NULL;
-
+  // бежим в цикле по записям в таблице SDT поисках заголовка FADT
   for (size_t i = 0; i < krsdt_len; i++) {
     /* Assume little endian */
     uint64_t fadt_pa = 0;
@@ -146,8 +171,11 @@ static void * acpi_find_table(const char * sign) {
 
     hd = mmio_map_region(fadt_pa, sizeof(ACPISDTHeader));
     /* Remap since we can obtain table length only after mapping */
-    hd = mmio_map_region(fadt_pa, hd->Length);
-
+    hd = mmio_remap_last_region(fadt_pa, hd, sizeof(ACPISDTHeader), krsdt->h.Length);
+    for (size_t i = 0; i < hd->Length; i++)
+      cksm = (uint8_t)(cksm + ((uint8_t *)hd)[i]);
+    if (cksm)
+      panic("ACPI table '%.4s' invalid", hd->Signature);
     if (!strncmp(hd->Signature, sign, 4)) return hd;
   }
 
@@ -159,11 +187,9 @@ static void * acpi_find_table(const char * sign) {
 FADT *
 get_fadt(void) {
   static FADT *kfadt;
-
   if (!kfadt) {
     kfadt = acpi_find_table("FACP");
   }
-
   return kfadt;
 }
 
@@ -274,27 +300,30 @@ hpet_get_main_cnt(void) {
 
 #define HPET_TN_TYPE_CNF (1 << 3)
 #define HPET_TN_INT_ENB_CNF (1 << 2)
-#define HPET_TN_VAL_SET_CNF (1 << 6)
+#define HPET_TN_VAL_SET_CNF (1 << 6) // 6 бит в регистре
 #define HPET_LEG_RT_CNF (1 << 1)
 
 void
 hpet_enable_interrupts_tim0(void) {
+  // LAB 5 code
   hpetReg->GEN_CONF |= HPET_LEG_RT_CNF;
   hpetReg->TIM0_CONF = (IRQ_TIMER << 9) | HPET_TN_TYPE_CNF | HPET_TN_INT_ENB_CNF | HPET_TN_VAL_SET_CNF;
   hpetReg->TIM0_COMP = hpet_get_main_cnt() + Peta / 2 / hpetFemto;
   hpetReg->TIM0_COMP = Peta / 2 / hpetFemto;
   irq_setmask_8259A(irq_mask_8259A & ~(1 << IRQ_TIMER));
+  // LAB 5 code end
 }
 
 void
 hpet_enable_interrupts_tim1(void) {
+  // LAB 5 code
   hpetReg->GEN_CONF |= HPET_LEG_RT_CNF;
   hpetReg->TIM1_CONF = (IRQ_CLOCK << 9) | HPET_TN_TYPE_CNF | HPET_TN_INT_ENB_CNF | HPET_TN_VAL_SET_CNF;
   hpetReg->TIM1_COMP = hpet_get_main_cnt() + 3 * Peta / 2 / hpetFemto;
   hpetReg->TIM1_COMP = 3 * Peta / 2 / hpetFemto;
   irq_setmask_8259A(irq_mask_8259A & ~(1 << IRQ_CLOCK));
+  // LAB 5 code end
 }
-
 void
 hpet_handle_interrupts_tim0(void) {
   pic_send_eoi(IRQ_TIMER);
@@ -309,8 +338,10 @@ hpet_handle_interrupts_tim1(void) {
 // Calculate CPU frequency in Hz with the help with HPET timer.
 // Hint: use hpet_get_main_cnt function and do not forget about
 // about pause instruction.
+
 uint64_t
 hpet_cpu_frequency(void) {
+  // LAB 5 code
   uint64_t time_res = 100;
   uint64_t delta = 0, target = hpetFreq / time_res;
 
@@ -323,7 +354,9 @@ hpet_cpu_frequency(void) {
 
   uint64_t tsc1 = read_tsc();
 
-  return (tsc1 - tsc0) * time_res;
+  return (tsc1 - tsc0) * time_res; 
+  // LAB 5 code end
+  // return 0;
 }
 
 uint32_t
@@ -338,8 +371,10 @@ pmtimer_get_timeval(void) {
 // Calculate CPU frequency in Hz with the help with ACPI PowerManagement timer.
 // Hint: use pmtimer_get_timeval function and do not forget that ACPI PM timer
 // can be 24-bit or 32-bit.
+
 uint64_t
 pmtimer_cpu_frequency(void) {
+  // LAB 5 code
   uint32_t time_res = 100;
   uint32_t tick0 = pmtimer_get_timeval();
   uint64_t delta = 0, target = PM_FREQ / time_res;
@@ -360,4 +395,6 @@ pmtimer_cpu_frequency(void) {
   uint64_t tsc1 = read_tsc();
 
   return (tsc1 - tsc0) * PM_FREQ / delta;
+  // LAB 5 code end
+  // return 0;
 }
